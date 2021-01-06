c34ab442a9e3b8388f107cf2ab3d383de32e9db9
chriss desmonds <chimbadzwa.christopherdesmond@gmail.com>
2021-01-06 10:57:17 +0200

    init commit

--- ghettoBasa/ghettoBasa/Controllers/ValuesController.cs
+++ ghettoBasa/ghettoBasa/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace ghettoBasa.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "Test" };
         }
 
         // GET api/values/5
