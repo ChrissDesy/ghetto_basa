306befa55a3e27cd4808aa9cdf50198d249746d2
chriss desmonds <chimbadzwa.christopherdesmond@gmail.com>
2021-01-17 22:54:27 +0200

    changes

--- ghettoBasa/IdentityServer/Startup.cs
+++ ghettoBasa/IdentityServer/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using IdentityServer4.Models;
 
 namespace IdentityServer
 {
@@ -15,6 +17,47 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var cn = "Server=localhost;Port=3306;Database=ghettobasa;Uid=root;Pwd=qris;";
+
+            var builder = services.AddIdentityServer()
+                .AddInMemoryApiResources(
+                    new ApiResource[]
+                    {
+                        new ApiResource( "ghettoBasa-api" )
+                        {
+                            ApiSecrets = {
+                                new Secret( "ghettoBAsa@2020".Sha256() )
+                            }
+                        }
+                    }
+                )
+                .AddInMemoryIdentityResources(
+                    new IdentityResource[]
+                    {
+                        new IdentityResources.OpenId()
+                    }
+                )
+                .AddInMemoryClients(
+                    new Client[]
+                    {
+                        new Client
+                        {
+                            ClientId = "ghettoBasa-frontend",
+                            ClientSecrets = {
+                                new Secret( "ghettoBAsaFront@2020".Sha256() )
+                            },
+
+                            AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                            AllowedScopes = {
+                                "ghettoBasa-api"
+                            },
+                        }
+                    }
+                );
+
+            builder.AddDeveloperSigningCredential();
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +67,14 @@ namespace IdentityServer
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.UseIdentityServer();
+
+            
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Identity Server!");
             });
         }
     }
